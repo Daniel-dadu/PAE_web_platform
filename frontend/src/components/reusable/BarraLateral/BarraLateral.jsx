d4f62b19f352fddfcfcafac46349e013bb90be28
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-25 01:30:21 -0500

    Added HTML code for BarraLateral component #9

--- frontend/src/components/reusable/BarraLateral/BarraLateral.jsx
+++ frontend/src/components/reusable/BarraLateral/BarraLateral.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+import btnInfo from './btnInfo'
+
 function BarraLateral() {
+    let user = "asesor"
+
   return (
-    <div>BarraLateral</div>
+    <div>
+        <div>
+            <a href="">
+                <img src="" alt="Logo PAE" />
+            </a>
+        </div>
+
+        <div>
+            <div>
+                {
+                btnInfo[user].buttons.map(
+                    (btn) => 
+                    <a href="">
+                    <img src={btn.image} alt={btn.text} />
+                    <p>{btn.text}</p>
+                    </a>
+                )
+                }
+            </div>
+        </div>
+
+        <div>
+            <a href="">
+                <img src="" alt="" />
+            </a>
+            <a href="">
+                <img src="" alt="" />
+            </a>
+        </div>
+
+    </div>
   )
 }
 
---
+++ frontend/src/components/reusable/BarraLateral/btnInfo.js
@@ -0,0 +1,66 @@
+import calendar from './calendar.png'
+
+export default {
+    "asesor": {
+        "buttons": [
+            {
+                "image": calendar,
+                "text": "Perfil"
+            },
+            {
+                "image": calendar,
+                "text": "Calendario"
+            },
+            {
+                "image": calendar,
+                "text": "Notificaciones"
+            }
+        ],
+        "backgroundColor": "#A1E01D",
+        "selectedButtonColor": "#1B5821"
+    },
+    "asesorado": {
+        "buttons": [
+            {
+                "image": "img",
+                "text": "Perfil"
+            },
+            {
+                "image": "img",
+                "text": "Calendario"
+            },
+            {
+                "image": "img",
+                "text": "Agendar asesoría"
+            },
+            {
+                "image": "img",
+                "text": "Notificaciones"
+            }
+        ],
+        "backgroundColor": "#50C7F1",
+        "selectedButtonColor": "#1B4858"
+    },
+    "directivo": {
+        "buttons": [
+            {
+                "image": "img",
+                "text": "Perfil"
+            },
+            {
+                "image": "img",
+                "text": "Calendario"
+            },
+            {
+                "image": "img",
+                "text": "Notificaciones"
+            },
+            {
+                "image": "img",
+                "text": "Administrar"
+            }
+        ],
+        "backgroundColor": "#4BE2BE",
+        "selectedButtonColor": "#1B5558"
+    }
+}
