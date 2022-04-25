70c7209d593b2c6ed33a1e35e42bc86591f1d1e9
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-24 22:50:29 -0500

    Improved routes import with routeIndex.js file #106

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import TemporalHome from './views/componentPreview/TemporalHome/TemporalHome';
-import LandingPage from './views/LandingPage/LandingPage';
-import Calendario from './views/commons/Calendario/Calendario';
-import  PerfilAsesor  from './views/asesor/perfil/PerfilAsesor';
-import  PerfilAsesorado  from './views/asesorado/perfil/PerfilAsesorado';
-import PerfilDirectivo from './views/directivo/perfil/PerfilDirectivo'
-import EditarPerfil from './views/commons/EditarPerfil/EditarPerfil'
-import DanielFlores from './views/componentPreview/DanielFlores/DanielFlores'
-import DanielMaldonado from './views/componentPreview/DanielMaldonado/DanielMaldonado'
-import DanielMunive from './views/componentPreview/DanielMunive/DanielMunive'
-import Ezequiel from './views/componentPreview/Ezequiel/Ezequiel'
-import Fernando from './views/componentPreview/Fernando/Fernando'
+import { TemporalHome, LandingPage, Calendario, PerfilAsesor, PerfilAsesorado, PerfilDirectivo, EditarPerfil, DanielFlores, DanielMaldonado, DanielMunive, Ezequiel, Fernando } from './routeIndex'
 
 const App = () => {
   return (
---
+++ frontend/src/routeIndex.js
@@ -0,0 +1,12 @@
+export {default as TemporalHome} from './views/componentPreview/TemporalHome/TemporalHome';
+export {default as LandingPage } from './views/LandingPage/LandingPage';
+export {default as Calendario } from './views/commons/Calendario/Calendario';
+export {default as  PerfilAsesor  } from './views/asesor/perfil/PerfilAsesor';
+export {default as  PerfilAsesorado  } from './views/asesorado/perfil/PerfilAsesorado';
+export {default as PerfilDirectivo } from './views/directivo/perfil/PerfilDirectivo'
+export {default as EditarPerfil } from './views/commons/EditarPerfil/EditarPerfil'
+export {default as DanielFlores } from './views/componentPreview/DanielFlores/DanielFlores'
+export {default as DanielMaldonado } from './views/componentPreview/DanielMaldonado/DanielMaldonado'
+export {default as DanielMunive } from './views/componentPreview/DanielMunive/DanielMunive'
+export {default as Ezequiel } from './views/componentPreview/Ezequiel/Ezequiel'
+export {default as Fernando } from './views/componentPreview/Fernando/Fernando'
