4ef99938713b1b94fe929ca4cfa2a66b95b97ea4
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-04-26 12:13:33 -0500

    correccion de nombre de los subdirectorios del directorio commoms

--- frontend/src/routeIndex.js
+++ frontend/src/routeIndex.js
@@ -1,10 +1,10 @@
 export {default as TemporalHome} from './views/componentPreview/TemporalHome/TemporalHome';
 export {default as LandingPage } from './views/LandingPage/LandingPage';
-export {default as Calendario } from './views/commons/Calendario/Calendario';
+export {default as Calendario } from './views/commons/calendario/Calendario';
 export {default as  PerfilAsesor  } from './views/asesor/perfil/PerfilAsesor';
 export {default as  PerfilAsesorado  } from './views/asesorado/perfil/PerfilAsesorado';
 export {default as PerfilDirectivo } from './views/directivo/perfil/PerfilDirectivo'
-export {default as EditarPerfil } from './views/commons/EditarPerfil/EditarPerfil'
+export {default as EditarPerfil } from './views/commons/editarPerfil/EditarPerfil'
 export {default as DanielFlores } from './views/componentPreview/DanielFlores/DanielFlores'
 export {default as DanielMaldonado } from './views/componentPreview/DanielMaldonado/DanielMaldonado'
 export {default as DanielMunive } from './views/componentPreview/DanielMunive/DanielMunive'
