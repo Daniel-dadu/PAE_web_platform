7c6824b5a5e360adfd488818646026e3fa0f147a
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-04-26 12:43:46 -0500

    correcion en el ruteo para garantizar usar el archivo correcto

--- frontend/src/routeIndex.js
+++ frontend/src/routeIndex.js
@@ -1,12 +1,12 @@
-export {default as TemporalHome} from './views/componentPreview/TemporalHome/TemporalHome';
-export {default as LandingPage } from './views/LandingPage/LandingPage';
-export {default as Calendario } from './views/commons/calendario/Calendario';
-export {default as  PerfilAsesor  } from './views/asesor/perfil/PerfilAsesor';
-export {default as  PerfilAsesorado  } from './views/asesorado/perfil/PerfilAsesorado';
-export {default as PerfilDirectivo } from './views/directivo/perfil/PerfilDirectivo'
-export {default as EditarPerfil } from './views/commons/editarPerfil/EditarPerfil'
-export {default as DanielFlores } from './views/componentPreview/DanielFlores/DanielFlores'
-export {default as DanielMaldonado } from './views/componentPreview/DanielMaldonado/DanielMaldonado'
-export {default as DanielMunive } from './views/componentPreview/DanielMunive/DanielMunive'
-export {default as Ezequiel } from './views/componentPreview/Ezequiel/Ezequiel'
-export {default as Fernando } from './views/componentPreview/Fernando/Fernando'
+export {default as TemporalHome} from './views/componentPreview/TemporalHome/TemporalHome.jsx';
+export {default as LandingPage } from './views/LandingPage/LandingPage.jsx';
+export {default as Calendario } from './views/commons/calendario/Calendario.jsx';
+export {default as  PerfilAsesor  } from './views/asesor/perfil/PerfilAsesor.jsx';
+export {default as  PerfilAsesorado  } from './views/asesorado/perfil/PerfilAsesorado.jsx';
+export {default as PerfilDirectivo } from './views/directivo/perfil/PerfilDirectivo.jsx'
+export {default as EditarPerfil } from './views/commons/editarPerfil/EditarPerfil.jsx'
+export {default as DanielFlores } from './views/componentPreview/DanielFlores/DanielFlores.jsx'
+export {default as DanielMaldonado } from './views/componentPreview/DanielMaldonado/DanielMaldonado.jsx'
+export {default as DanielMunive } from './views/componentPreview/DanielMunive/DanielMunive.jsx'
+export {default as Ezequiel } from './views/componentPreview/Ezequiel/Ezequiel.jsx'
+export {default as Fernando } from './views/componentPreview/Fernando/Fernando.jsx'
