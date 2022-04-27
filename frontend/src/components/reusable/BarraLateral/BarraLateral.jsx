52905d8b7ed05beaa692a1277783a499a4106c65
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-26 23:42:57 -0500

    Added selection of btn #9

--- frontend/src/components/reusable/BarraLateral/BarraLateral.jsx
+++ frontend/src/components/reusable/BarraLateral/BarraLateral.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './BarraLateral.css'
 
 import { BiCalendar, BiEdit } from 'react-icons/bi'
@@ -19,9 +19,18 @@ const routeBtn = (image, user) => {
 function BarraLateral() {
 
     // IMPORTANTE: Usar estos strings no es muy eficiente para el componente final, evaluar si se cambia por números 
-    let user = "directivo"
+    let userProp = "directivo"
     let tema = "claro"
     let idioma = "espanol"
+    let viewProp = "notificaciones" // Vista actual en la que se muestra la barra
+    /*
+    "perfil"
+    "calendario"
+    "notificaciones"
+    "agendarAsesoria"
+    "administrar"
+    */
+
     const barIconSize = 80
 
   return (
@@ -32,21 +41,72 @@ function BarraLateral() {
             </a>
         </div>
 
-        <div className='barra-container' style={{backgroundColor: btnInfo[user].backgroundColor }}>
+        <div className='barra-container' style={{backgroundColor: btnInfo[userProp].backgroundColor }}>
             {
-            btnInfo[user].buttons.map((btn) => 
-                <div className='barra_button' style={{height: (user === "asesor") ? '33%' : '25%'}}>
-                <a href={ routeBtn(btn.image, user) }>
-                {
-                (typeof btn.image === "string") ? <img src={btn.image} alt="" />
-                : (btn.image === 1) ? <BiCalendar className='icon' size={barIconSize}/>
-                : (btn.image === 2) ? <MdNotificationsNone className='icon' size={barIconSize}/>
-                : (btn.image === 3) ? <BsPeople className='icon' size={barIconSize}/> 
-                : <BiEdit className='icon' size={barIconSize}/>
+            btnInfo[userProp].buttons.map((btn) => {
+                let isImageString = typeof btn.image === "string"
+                let heightBtn = (userProp === "asesor") ? '33%' : '25%'
+
+                if(isImageString){
+                    let perfilSelected = viewProp === "perfil"
+                    return (
+                    <div className={'barra_button' + (perfilSelected ? ' barra_button-selected' : '') } style={{height: heightBtn}}>
+                    <a href={ btn.user === "asesor" ? "/perfilAsesor" : userProp === "asesorado" ? "/perfilAsesorado" : "/perfilDirectivo" }>
+                    <img src={btn.image} alt="Perfil" className={'profile-img' + (perfilSelected ? ' selected_icon' : '')} />
+                    <p className={'btn-text' + (perfilSelected ? ' selected_icon' : '')}>{btn.text}</p>
+                    </a>
+                    </div>
+                    )
+                } else if(btn.image === 1) {
+                    let calendarioSelected = viewProp === "calendario"
+                    return (
+                    <div className={'barra_button' + (calendarioSelected ? ' barra_button-selected' : '')} style={{height: heightBtn}}>
+                    <a href="/calendario">
+                    <BiCalendar className={'icon_bar' + (calendarioSelected ? ' selected_icon' : '')} size={barIconSize}/>
+                    <p className={'btn-text' + (calendarioSelected ? ' selected_icon' : '')}>{btn.text}</p>
+                    </a>
+                    </div>
+                    )
+                } else if(btn.image === 2){
+                    let notificacionesSelected = viewProp === "notificaciones"
+                    return <div className={'barra_button' + (notificacionesSelected ? ' barra_button-selected' : '')} style={{height: heightBtn}}>
+                        <a href="/notificaciones">
+                        <MdNotificationsNone className={'icon_bar' + (notificacionesSelected ? ' selected_icon' : '')} size={barIconSize}/>
+                        <p className={'btn-text' + (notificacionesSelected ? ' selected_icon' : '')}>{btn.text}</p>
+                        </a>
+                        </div>
+                } else if(btn.image === 3){
+                    let agendarSelected = viewProp == "agendarAsesoria"
+                    return <div className={'barra_button' + (agendarSelected ? ' barra_button-selected' : '')} style={{height: heightBtn}}>
+                        <a href="/agendarAsesoria">
+                        <BsPeople className={'icon_bar' + (agendarSelected ? ' selected_icon' : '')} size={barIconSize}/> 
+                        <p className={'btn-text' + (agendarSelected ? ' selected_icon' : '')}>{btn.text}</p>
+                        </a>
+                        </div>
+                } else {
+                    let administrarSelected = viewProp == "administrar"
+                    return <div className={'barra_button' + (administrarSelected ? ' barra_button-selected' : '')} style={{height: heightBtn}}>
+                        <a href="/administrar">
+                        <BiEdit className={'icon_bar' + (administrarSelected ? ' selected_icon' : '')} size={barIconSize}/>
+                        <p className={'btn-text' + (administrarSelected ? ' selected_icon' : '')}>{btn.text}</p>
+                        </a>
+                        </div>
                 }
-                <p>{btn.text}</p>
+                /*
+                return <div className='barra_button' style={{height: heightBtn}}>
+                <a href={ routeBtn(btn.image, userProp) }>
+                {
+                (isImageString) ? <img src={btn.image} alt="" />
+                : (btn.image === 1) ? <BiCalendar className='icon_bar selected_icon' size={barIconSize}/>
+                : (btn.image === 2) ? <MdNotificationsNone className='icon_bar' size={barIconSize}/>
+                : (btn.image === 3) ? <BsPeople className='icon_bar' size={barIconSize}/> 
+                : <BiEdit className='icon_bar' size={barIconSize}/>
+                }
+                <p className='btn-text selected_icon'>{btn.text}</p>
                 </a>
                 </div>
+                */
+            }
             )
             }
         </div>
