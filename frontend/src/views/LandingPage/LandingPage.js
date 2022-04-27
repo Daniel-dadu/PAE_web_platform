d29b56485b3d0b4008e3ede3d1cf9c24929bfe41
danoman17 <daniflores374@gmail.com>
2022-04-27 06:42:20 -0500

    landing page pages

--- frontend/src/views/LandingPage/LandingPage.js
+++ frontend/src/views/LandingPage/LandingPage.js
@@ -1,30 +1,112 @@
 import React from 'react'
-import { Nav, NavLink, Bars, NavMenu, NavBtnLink } from './NavbarElements';
+import { Nav, NavLink, Bars, NavMenu } from './NavbarElements';
+import './landingPage.css';
 
 export const LandingPage = () => {
   return (
     <>
        <Nav>
            <NavLink to="/">
-                <h1>Logo</h1>
+                <img className='logo-pae' src={ require('../../assets/PAE-logo.png') } alt='PAE'  />
            </NavLink>
            <Bars/>
            <NavMenu>
-               <NavLink to="/quienesSomos">
+
+                <a href='#quienesSomos'>
+                    ¿Quiénes Somos?
+                </a>
+                <a href='#funcionamiento'>
+                    Funcionamiento
+                </a>
+                <a href='#objetivos'>
+                    Objetivos
+                </a>
+                <a href='#equipo-trabajo'>
+                    Equipo de trabajo
+                </a>
+                
+{/* 
+               <NavLink to="#quienesSomos" activeStyle>
                    ¿Quiénes Somos?
                </NavLink>
-               <NavLink to="/funcionamiento">
+               <NavLink to="#funcionamiento" activeStyle>
                    Funcionamiento
                </NavLink>
-               <NavLink to="/Objetivos">
+               <NavLink to="#objetivos" activeStyle>
                    Objetivos
                </NavLink>
-               <NavLink to="/equipoTrabajo">
+               <NavLink to="#equipo-trabajo" activeStyle>
                    Equipo de trabajo
-               </NavLink>
+               </NavLink> */}
            </NavMenu>
            
        </Nav>
+
+       <section className='quienes-somos' id='quienesSomos'>
+
+            <div className='izq'>
+               <h1>¿Qué es el Programa de Asesor Estudiante?</h1>
+               <div className='texto-derecha'>
+                    <b>Asesorias</b>
+                    <p>
+                        Alumnos solidarios que
+                        brindan asesorías a sus
+                        compañeros.                        
+                    </p>
+                    <b> Servicio Social </b>
+                    <p>
+                        Clases de inglés a niños de
+                        primaria de escasos
+                        recursos.
+                    </p>
+               </div>
+               <div className='btn-contenedor'>
+                    <button> Saber más </button>
+               </div>
+            </div>
+
+
+            <div className='der'>
+                <img className='img-quienes-somos' src={ require('../../assets/estudiando-img.png') } />
+            </div>
+
+
+       </section>
+
+       <section className='funcionamiento' id='funcionamiento'>
+        
+            
+
+
+            <div className='izq'>
+                <svg className='blob-funcionamiento' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                    <path className='blob-funcionamiento' fill="#455A64" d="M57.1,-57.2C70,-44.1,73.9,-22.1,72.8,-1.1C71.7,19.8,65.6,39.6,52.6,54.9C39.6,70.2,19.8,81,-1.5,82.5C-22.9,84.1,-45.8,76.4,-56.3,61.1C-66.8,45.8,-64.8,22.9,-64.1,0.8C-63.3,-21.4,-63.7,-42.7,-53.2,-55.9C-42.7,-69,-21.4,-73.9,0.3,-74.2C22.1,-74.6,44.1,-70.4,57.1,-57.2Z" transform="translate(100 100)" />
+                </svg>
+               <h1> Funcionamiento del Programa </h1>
+               <p>
+               El programa de asesor estudiante funciona de manera sencilla y rápida, trayendo como beneficios, entre otros, la oportunidad de conocer nuevas personas y mejoar calificaciones. 
+               </p>
+
+            </div>
+            
+            <div className='der'>
+                <img className='img-quienes-somos' src={ require('../../assets/estudiando2-img.png') } />
+            </div>
+       </section>
+
+       <section className='pasos-inscripcion'>
+               
+
+
+       </section>
+
+       <section className='objetivos' id='objetivos'>
+            objetivos
+       </section>
+
+       <section className='equipo-trabajo' id='equipo-trabajo'>
+            Equipo de trabajo
+       </section>
     </>
   )
 }
--- frontend/src/views/LandingPage/NavbarElements.js
+++ frontend/src/views/LandingPage/NavbarElements.js
@@ -4,32 +4,55 @@ import { FaBars } from 'react-icons/fa';
 
 
 export const Nav = styled.nav`
-    background: #000;
-    height: 80px;
+    background: #FFFFFF;
+    height: 68px;
     display: flex;
     justify-content: space-between;
-    padding: 0.5rem calc(( 100vw - 1000px ) / 2);
-    z-index: 10;
+    padding: 0.5rem calc(( 100vw - 1500px ) / 2);
+    z-index: 100;
+    -webkit-box-shadow: 1px 8px 15px 5px rgba(0,0,0,0.18); 
+    box-shadow: 1px 8px 15px 5px rgba(0,0,0,0.18);
+    width: 70%;
+    position: sticky;
+    top: 0;
+
+
+    @media screen and (max-width: 768px) {
+        width: 100%;
+    }
+
+    @media only screen and (max-width: 992px) {
+        padding: 0.5rem 2rem;
+    }
+
 `
 
 export const NavLink = styled(Link)`
-    color: #fff;
+    color: #000;
     display: flex;
     align-items: center;
     text-decoration: none;
     padding: 0 1rem;
     height: 100%;
     cursor: pointer;
+    font-size: 1.2rem;
+    font-weight: 400;
+    font-family: 'Dosis', sans-serif;
 
-    &.active {
-        color: #14cdfc;
+    @media only screen and (max-width: 992px) {
+        font-size: 0.9rem;
+        padding: 0 0.5rem;
     }
 
+
+    &.active {
+        color: #15cdfc;
+    }
 `
 
 export const Bars = styled(FaBars)`
     display: none;
-    color: #fff;
+    color: #000;
 
     @media screen and (max-width: 768px) {
         display: block;
@@ -45,9 +68,20 @@ export const Bars = styled(FaBars)`
 export const NavMenu = styled.div`
     display: flex;
     align-items: center;
-    margin-top: -24px;
+    /* margin-top: -5px; */
 
     @media screen and (max-width: 768px) {
+        display: none;
+    }
+
+`
+
+export const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+
+    @media screen and (max-width: 768px){
         display: none;
     }
 `
