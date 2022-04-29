885b1799141a14320a195cab0de6cfcf3293b47d
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-28 21:25:28 -0500

    Using json to get data #30

--- frontend/src/views/asesorado/agendarAsesoriaUF/AgendarAsesoriaUF.jsx
+++ frontend/src/views/asesorado/agendarAsesoriaUF/AgendarAsesoriaUF.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './AgendarAsesoriaUF.css'
 
-import { AgendarAsesoria } from '../../../routeIndex'
+import info from './info.json'
+
+import { AgendarAsesoria, CampoSeleccionarEnListaDesplegable } from '../../../routeIndex'
 
 function AgendarAsesoriaUF() {
+
   return (
     <AgendarAsesoria showAtrasBtn={false} showTarjetaMaestraMini={true} sizeTarjetaMaestraMini="normal">
-        Hola
+        <h3>Carrera</h3>
+        <CampoSeleccionarEnListaDesplegable size="medium" options={info.semestre}/>
+        <h3>Semestre</h3>
+        <CampoSeleccionarEnListaDesplegable size="small" options={info.carrera}/>
     </AgendarAsesoria>
   )
 }
