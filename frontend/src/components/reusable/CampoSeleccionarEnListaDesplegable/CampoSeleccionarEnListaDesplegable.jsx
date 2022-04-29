f85f75de255e40a8622f5e2d8c52120fea3a4f45
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-28 23:07:16 -0500

    Finished AgendarAsesoriaUF view #30

--- frontend/src/components/reusable/CampoSeleccionarEnListaDesplegable/CampoSeleccionarEnListaDesplegable.jsx
+++ frontend/src/components/reusable/CampoSeleccionarEnListaDesplegable/CampoSeleccionarEnListaDesplegable.jsx
@@ -6,15 +6,19 @@ import { FiChevronDown } from 'react-icons/fi';
 // Descripción de las propedades
 /*
 size: se recibe un string que puede ser "small", "medium" o "big"
+idList: se recibe un string que relaciona el input con la lista. Puede ser cualquier conjunto de caracteres como "miLista"
 options: se recibe un array de strings con las opciones que se deben mostrar
+
+EJEMPLO:
+<CampoSeleccionarEnListaDesplegable size="small" options={info.semestre} idList="carrera"/>
 */
 
-function CampoSeleccionarEnListaDesplegable({ size, options }) {
+function CampoSeleccionarEnListaDesplegable({ size, idList, options }) {
   return (
     <div className="container_cseld">
-      <input list="browsers" name="browser" id="browser" style={{width: size === "small" ? "100px" : size === "medium" ? "250px" : "350px"}}/>
+      <input list={idList} style={{width: size === "small" ? "100px" : size === "medium" ? "250px" : "350px"}}/>
       <FiChevronDown className="arrow_down" size={30} />
-      <datalist id="browsers">
+      <datalist id={idList}>
         {options.map((opcion) => (
           <option value={opcion} />
         ))}
--- frontend/src/views/asesorado/agendarAsesoriaUF/AgendarAsesoriaUF.jsx
+++ frontend/src/views/asesorado/agendarAsesoriaUF/AgendarAsesoriaUF.jsx
@@ -9,10 +9,16 @@ function AgendarAsesoriaUF() {
 
   return (
     <AgendarAsesoria showAtrasBtn={false} showTarjetaMaestraMini={true} sizeTarjetaMaestraMini="normal">
-        <h3>Carrera</h3>
-        <CampoSeleccionarEnListaDesplegable size="medium" options={info.semestre}/>
-        <h3>Semestre</h3>
-        <CampoSeleccionarEnListaDesplegable size="small" options={info.carrera}/>
+      <div className='container_out_aauf'>
+        <div className='container_in_aauf'>
+          <h3>Carrera</h3>
+          <CampoSeleccionarEnListaDesplegable size="medium" options={info.carrera} idList="semestre"/>
+          <h3>Semestre</h3>
+          <CampoSeleccionarEnListaDesplegable size="small" options={info.semestre} idList="carrera"/>
+          <h3>Unidad de formación</h3>
+          <CampoSeleccionarEnListaDesplegable size="medium" options={info.unidadFormacion} idList="unidadFormacion"/>
+        </div>
+      </div>
     </AgendarAsesoria>
   )
 }
