39c9d6180140f8e3b4892c955d99489e5bca6f01
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-05-05 17:01:44 -0500

    Proxy funcional, las ips que redirecionan al servidor tienen que estar hardcordeadas #138 #143

--- backend/client-builder/client-builder.py
+++ backend/client-builder/client-builder.py
@@ -2,19 +2,26 @@ import os
 from flask import Flask
 from flask import redirect
 from waitress import serve
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def build():
+    print("Builder start ", datetime.now())
     os.chdir(os.path.expanduser("~/PAE_web_platform/frontend"))
+    print("Builder change directory")
     os.system("npm install")
     if os.path.isdir("build"):
+        print("Builder delete old build")
         os.system("rm -rf build")
     os.system("npm run build")
+    print("Builder create new builder")
     os.system("sudo reloadClient")
-    return redirect("http://192.168.1.74:81", code=302)
+    print("Builder reload builder ",  datetime.now())
+    #la ip de redirect tiene que ser la ip del servidor harcordeada
+    return redirect("http://10.50.89.169", code=302)
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=1024)
