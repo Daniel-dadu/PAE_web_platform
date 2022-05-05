88a70ba26f21458ebdfa3e2ac7d2d076539d5495
Daniel-dadu <dadu9494@gmail.com>
2022-05-05 02:11:57 -0500

    Defined endpoint for GET UF from specific carrera and semestre #188

--- backend/microservices/Asesoria/queries.js
+++ backend/microservices/Asesoria/queries.js
@@ -19,11 +19,22 @@ const getCarreras = (_request, response) => {
 }
 
 const getUF_carreraSemestre = (request, response) => {
-  pool.query('SELECT * FROM "Carrera"', (error, results) => {
+  // Parámetros proporcionados en la request HTTP
+  const carrera = request.params.carrera
+  const semestre = request.params.semestre
+
+  const consulta = 'SELECT "UnidadFormacion"."idUF" AS "claveUF", "nombreUF" FROM "UnidadFormacion", "UnidadFormacionCarrera" WHERE "UnidadFormacionCarrera"."idUF" = "UnidadFormacion"."idUF" AND "UnidadFormacionCarrera"."idCarrera" = $1 AND "UnidadFormacion"."semestre" = $2'
+
+  pool.query(consulta, [carrera, semestre], (error, results) => {
     if (error) {
       throw error
+    } else if(!results.rows.length) {
+      // En caso de que no se encuentre ninguna UF con ese semestre y carrera
+      response.status(404).json({"asesoria-UF_carreraSemestre": "No se encontraron UFs para esa carrera y semestre"})
+    } else {
+      // Se regresa la lista de las UFs de esa carrera y semestre
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 
