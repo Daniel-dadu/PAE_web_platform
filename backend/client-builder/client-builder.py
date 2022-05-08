129bca85fc354a1bb9000e20e890b74d2e5fdfd1
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-05-07 23:22:28 -0500

    Configuracion de builder y proxy para el servidor en azure

--- backend/client-builder/client-builder.py
+++ backend/client-builder/client-builder.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def build():
     print("Builder start ", datetime.now())
-    os.chdir(os.path.expanduser("~/PAE_web_platform/frontend"))
+    os.chdir(os.path.expanduser("/var/www/pae/client/frontend"))
+    os.system("git checkout develop")
+    os.system("git pull origin develop")
     print("Builder change directory")
     os.system("npm install")
     if os.path.isdir("build"):
@@ -21,7 +23,7 @@ def build():
     os.system("sudo reloadClient")
     print("Builder reload builder ",  datetime.now())
     #la ip de redirect tiene que ser la ip del servidor harcordeada
-    return redirect("http://10.50.89.169", code=302)
+    return redirect("http://20.225.209.57", code=302)
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=1024)
