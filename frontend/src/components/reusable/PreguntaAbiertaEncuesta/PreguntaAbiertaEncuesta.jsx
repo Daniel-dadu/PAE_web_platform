0757ea012fabaad014f99ac8f9a38dc4f1995900
danie@LAPTOP-MQGLDRRG <a01734205tesm.mx>
2022-05-09 15:29:12 -0500

    hecho con merge y correeccion de danielmunive.jsx #168

--- frontend/src/components/reusable/PreguntaAbiertaEncuesta/PreguntaAbiertaEncuesta.jsx
+++ frontend/src/components/reusable/PreguntaAbiertaEncuesta/PreguntaAbiertaEncuesta.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 import './PreguntaAbiertaEncuesta.css'
 import {  CampoTextoGrande } from '../../../routeIndex'
 
+
+
+// eslint-disable-next-line no-lone-blocks
+{/* <PreguntaAbiertaEncuesta 
+
+preguntaAbierta={'Aqui pon la pregunta que quieras'}>
+
+</PreguntaAbiertaEncuesta> */}
+
 function PreguntaAbiertaEncuesta({preguntaAbierta}) {
   return (
     <div className='contenedorPreguntaAbiertaEncuesta'>
--- frontend/src/views/componentPreview/DanielMunive/DanielMunive.jsx
+++ frontend/src/views/componentPreview/DanielMunive/DanielMunive.jsx
@@ -1,38 +1,11 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
+import React from 'react'
 import { PreguntaAbiertaEncuesta }  from '../../../routeIndex'
 
-
-
-
-=======
-import React from 'react'
-import { BotonAdministrarInformacion }  from '../../../routeIndex'
-
 import Logo from "./imgPrueba/RespuestasEncuestas.png";
->>>>>>> develop
 
 import './Munive.css'
 function DanielMunive() {
 
-<<<<<<< HEAD
-
-
-  return (
-    <div // Div de prueba para la imagen
-         class = 'containerImg'
-         style = {{
-           width: '100%',
-           height: '150px',
-           marginLeft: 'auto',
-           marginRight: 'auto'
-         }}
-       >
-
-         <PreguntaAbiertaEncuesta preguntaAbierta='Deja tu Comentario :D'></PreguntaAbiertaEncuesta>
-         
-       </div>
-=======
 
   return (
     <>
@@ -47,17 +20,15 @@ function DanielMunive() {
     >
 
 
-      <BotonAdministrarInformacion 
-      onClick={() => {alert('Me diste click :)')}} 
-      Imagen={Logo}
-      children='Respuestas de encuestas a asesorados'>
+      <PreguntaAbiertaEncuesta 
 
-      </BotonAdministrarInformacion>
+      preguntaAbierta={'Deja tu comentario aqui'}>
+
+      </PreguntaAbiertaEncuesta>
 
     </div>
   </>
 
->>>>>>> develop
 
   )
 }
