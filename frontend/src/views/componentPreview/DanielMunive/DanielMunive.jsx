0faa36a39f7fe1af8ad6e3f25de4c571b13eb66f
danie@LAPTOP-MQGLDRRG <a01734205tesm.mx>
2022-05-09 10:19:49 -0500

    Creacion de branch #170

--- frontend/src/views/componentPreview/DanielMunive/DanielMunive.jsx
+++ frontend/src/views/componentPreview/DanielMunive/DanielMunive.jsx
@@ -1,38 +1,29 @@
 import React, { useState } from 'react'
-import { PopUpInformacionAsesoria }  from '../../../routeIndex'
 
-import  Modal from '../../../components/reusable/PopUpInformacionAsesoria/Modal';
 
-import EjemploJSON from './pruebasGrandes.json'
 
-import './Munive.css'
 function DanielMunive() {
 
-  const [active, setActive] = useState(false);
-
-  const toggle = () => {
-    setActive(!active)
-  }
-
   return (
-    <div className='.fullPantalla'> 
+    <>
+      <div // Div de prueba para la imagen
+        class = 'containerImg'
+        style = {{
+          width: '100%',
+          height: '150px',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }}
+      >
 
-      <button style={{
-        position: 'absolute',
-        top: '50%',
-        padding: 10,
-      }} onClick={toggle}>Open Modal</button>
+        
 
-
-      <Modal active={active} toggle={toggle}>
-      <PopUpInformacionAsesoria  userTypePopUpAsesoria = "alumno" infoAsesoria = {EjemploJSON} estado={toggle}></PopUpInformacionAsesoria> 
-      </Modal>
-
-    
-
-    </div>
-
+      </div>
+    </>
   )
+
+
+  
 }
 
 export default DanielMunive
