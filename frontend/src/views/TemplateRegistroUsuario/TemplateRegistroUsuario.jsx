417796671144b1880f0f5e8d6eacbd8c0ab6aa13
danie@LAPTOP-MQGLDRRG <a01734205tesm.mx>
2022-05-10 15:55:01 -0500

    vista finalizada y agregar botones a template #196

--- frontend/src/views/TemplateRegistroUsuario/TemplateRegistroUsuario.jsx
+++ frontend/src/views/TemplateRegistroUsuario/TemplateRegistroUsuario.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../../index.css'
 import './TemplateRegistroUsuario.css'
-import { TarjetaMaestra, BarraProgreso } from '../../routeIndex'
+import {BotonSencillo, TarjetaMaestra, BarraProgreso } from '../../routeIndex'
+
+import { useNavigate } from "react-router-dom";
 
 const TemplateRegistroUsuario = ({
     progressBarJSON,
-    children
+    children, showAtrasBtn, btnAtrasRoute, btnSiguienteRoute
 }) => {
+
+    let navigate = useNavigate()
+    const routeChange = route => navigate(`/${route}`);
 
     return(
         <>
@@ -21,9 +26,24 @@ const TemplateRegistroUsuario = ({
             <br />
             <TarjetaMaestra>
                 {children}
+
+                <div className='container_navButtons_RegistroUsuario'>
+            <div className='.btn_right_registro'>
+                <BotonSencillo onClick = {typeof btnAtrasRoute === 'string' ? () => routeChange(btnAtrasRoute) : "accionConBackend"} backgroundColor='azulCielo' size='largo'>
+                    Atras
+                </BotonSencillo>
+            </div> 
+            <div >
+                <BotonSencillo onClick={typeof btnSiguienteRoute === 'string' ? () => routeChange(btnSiguienteRoute) : "accionConBackend"} backgroundColor='verde' size='largo'>
+                    Siguiente
+                </BotonSencillo>
+            </div>
+        </div>
             </TarjetaMaestra>
 
         </div>
+
+
         </>
     );
 
--- frontend/src/views/asesor/perfil/registroAsesorCondiciones/RegistroAsesorCondiciones.jsx
+++ frontend/src/views/asesor/perfil/registroAsesorCondiciones/RegistroAsesorCondiciones.jsx
@@ -1,24 +1,80 @@
 import React from 'react'
 
-import { TemplateRegistroUsuario } from '../../../../routeIndex'
+import { TemplateRegistroUsuario, BotonSencillo } from '../../../../routeIndex'
 
 
 import "./RegistroAsesorCondiciones.css"
 
+import Condiciones from './condiciones.json'
+
+let progressBar = {
+  "currentStep": 3,
+  "steps": [
+      {
+          "name" : "Datos Generales",
+          "state": true,
+          "next": "enable",
+          "path" : "Ruta"
+        }, 
+        {
+          "name" : "Horario",
+          "state": true,
+          "next": "enable",
+          "path" : "Ruta"
+        },
+        {
+          "name" : "Unidad de Formacion",
+          "state": true,
+          "next": "enable",
+          "path" : "Ruta"
+        },
+        {
+          "name" : "Consideraciones Finales",
+          "state": null,
+          "next": "enable",
+          "path" : "./registroAsesorCondiciones"
+        },
+        {
+          "name" : "Confirmación",
+          "state": null,
+          "next": "enable",
+          "path" : "Ruta"
+        }
+  ]
+}
+
 function RegistroAsesorCondiciones() {
   return (
-    <div>
         
-         <TemplateRegistroUsuario> 
+         <TemplateRegistroUsuario 
+         progressBarJSON = {progressBar}
+          btnAtrasRoute="./calendario"
+          btnSiguienteRoute="./calendario"> 
              
-         <div >
-            <h1 >Agendar asesorías</h1>
+         <div className='bloq_condicionesAsesor'>
+            <h1 className='campo_RegistroAsesorCondiciones'> CAMPO 4: Consideraciones Finales </h1>
+
+            <h1 className='condicionesAsesor_texto'> Acepta los terminos y condiciones de uso </h1>
+          
+          </div>
+
+        <div className='condicionesAsesor'>
+
+        <div className='condiciones_condicionesAsesor'> <p className='texto_condiciones_condicionesAsesor'> {Condiciones.Condiciones} </p> </div>
+          
+          <div> 
+            <input type="checkbox" id="condiciones" name="condiciones" value="AceptoCondiciones"/>
+            <label for="condiciones" style={{fontSize: "20px"}} > Acepto los terminos y condiciones</label>
+          </div>
+
+
+
         </div>
-        
-         </TemplateRegistroUsuario> 
 
         
-    </div>
+        
+         </TemplateRegistroUsuario>
+
   )
 }
 
