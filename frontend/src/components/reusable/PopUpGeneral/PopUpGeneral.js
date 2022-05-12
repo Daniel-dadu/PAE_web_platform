4e6ecad8ba3e8d4ba9766b05727c5fdd173e2f2e
danoman17 <daniflores374@gmail.com>
2022-05-12 00:22:22 -0500

    init PopUpGeneral #182

---
+++ frontend/src/components/reusable/PopUpGeneral/PopUpGeneral.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PopUpGeneral = () => {
+  return (
+    <>
+        <div className='contenedorGeneral-PopUpGeneral'>
+            <div className='parte-arriba-PopUpGeneral'>
+                <p className='texto-PopUpGeneral'> Tu reserva se realizó exitosamente </p>
+            </div>
+            <div>
+                <a href='#'> Entendido </a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default PopUpGeneral
