3f3a00bdf429a2d0628da3a37641a2229313e1f3
Daniel Flores Rodríguez <danoman@Daniels-MacBook-Air.local>
2022-05-12 09:32:19 -0500

    Terminado primer diseño de PopUpGenral #182

--- frontend/src/components/reusable/PopUpGeneral/PopUpGeneral.js
+++ frontend/src/components/reusable/PopUpGeneral/PopUpGeneral.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { BotonSencillo } from '../../../routeIndex';
+import { Template, BarraProgreso, TarjetaMaestraMini } from '../../../routeIndex'
+import './popUpGeneral.css';
 
-const PopUpGeneral = () => {
+
+
+const PopUpGeneral = (  ) => {
+
+  const saludar = () => {
+    window.alert("hola");
+  }
+
   return (
     <>
+        
         <div className='contenedorGeneral-PopUpGeneral'>
-            <div className='parte-arriba-PopUpGeneral'>
-                <p className='texto-PopUpGeneral'> Tu reserva se realizó exitosamente </p>
-            </div>
-            <div>
-                <a href='#'> Entendido </a>
-            </div>
+
+          <div className='contenedor-PopUpGeneral'>
+              <div className='parte-arriba-PopUpGeneral'>
+                  <p className='texto-PopUpGeneral'> Tu reserva se realizó exitosamente </p>
+              </div>
+              <div className='parte-abajo-PopUpGeneral'>
+                  <BotonSencillo onClick={ saludar } backgroundColor="verde" size="normal"  children="Entendido" />
+              </div>
+          </div>
+
         </div>
+  
+        
     </>
   )
 }
--- frontend/src/views/componentPreview/DanielFlores/DanielFlores.js
+++ frontend/src/views/componentPreview/DanielFlores/DanielFlores.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import PopUpGeneral from '../../../components/reusable/PopUpGeneral/PopUpGeneral'
+import { Template, BarraProgreso, TarjetaMaestraMini } from '../../../routeIndex'
 const DanielFlores = () => {
   return (
     <>
-        
+        <Template view="perfil">
+
+            <PopUpGeneral/>
+
+        </Template>
     </>
   )
 }
