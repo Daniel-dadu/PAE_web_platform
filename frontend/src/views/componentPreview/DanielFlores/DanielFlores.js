283971d41406c13ab53aa21acb225dd00d7247c9
Daniel Flores Rodríguez <danoman@Daniels-MacBook-Air.local>
2022-05-12 23:21:40 -0500

    finished PopUpGeneral #182

--- frontend/src/views/componentPreview/DanielFlores/DanielFlores.js
+++ frontend/src/views/componentPreview/DanielFlores/DanielFlores.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PopUpGeneral from '../../../components/reusable/PopUpGeneral/PopUpGeneral'
 import { Template, BarraProgreso, TarjetaMaestraMini } from '../../../routeIndex'
+
+
 const DanielFlores = () => {
+
+  const [state, setState] = useState(true);  
+
+  const handleBtn = () => {
+    setState(!state);
+  }
+
   return (
     <>
         <Template view="perfil">
-
-            <PopUpGeneral/>
-
+            <PopUpGeneral tipoPopUpGeneral= { false } state={ state } funcion={ handleBtn } />
+            <button onClick={ handleBtn }> Prueba </button>
         </Template>
     </>
   )
