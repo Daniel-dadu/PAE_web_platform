03a1ba9f9ad0d2baf0e3626f1a78138325a626bd
danie@LAPTOP-MQGLDRRG <a01734205tesm.mx>
2022-05-16 16:58:20 -0500

    IF panel administar Usuario para Asesor(boton link) y Asesorado #207

--- frontend/src/views/commons/administrarUsuariosDirectivos/AdministrarUsuariosDirectivos.jsx
+++ frontend/src/views/commons/administrarUsuariosDirectivos/AdministrarUsuariosDirectivos.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React,{useState}  from 'react'
 import '../../../index.css'
 import './AdministrarUsuariosDirectivos.css'
 import { useNavigate } from "react-router-dom";
-import { Template, ListaUsuarios, BotonSencillo} from '../../../routeIndex'
+import { Template, ListaUsuarios, BotonSencillo, BotonConImagen } from '../../../routeIndex'
 
-const AdministrarUsuariosDirectivos = () => {
+import { FaCopy} from 'react-icons/fa'
+
+
+const AdministrarUsuariosDirectivos = (userTypeUsuariosDirectivos) => {
 
     let navigate = useNavigate()
     const routeChange = route => navigate(`/${route}`);
 
+    const [active, setActive] = useState(false);
+  
+    const toggle = () => {
+        setActive(!active)
+    }
+    
+    window.toggle = toggle;
+
+
+
     return(
         <>
         <Template view = "administrar">
+
+
+        {(userTypeUsuariosDirectivos === 'Asesorados') ? 
+
+        <h1> Administrar asesorados </h1>
+        
+        :
+
+        <div className='btn_AdministrarUsuariosDirectivos'>
+
+        <h1> Administrar asesores </h1>
+
+        <div className='botonCopiarLinkRegistro'>
+            <BotonConImagen 
+            onClick={'Hola'} 
+            backgroundColor='grisClaro'
+            size="largo" 
+            Image={FaCopy} >
+                <div className='textoDeBoton'>Generar Nuevo Link de Registro</div> 
+            </BotonConImagen>
+        </div>
+
+        </div> 
+        
+        }
+ 
 
             <div className = 'divListaUsuarios'>
                 <ListaUsuarios></ListaUsuarios>
