51f5d16d9ee3a992b5ba7a223530641b2b6eb757
Daniel-dadu <dadu9494@gmail.com>
2022-05-18 16:52:54 +0000

    Commented imageCompressor.js file

--- frontend/src/assets/reusableFunctions/imageCompressor.js
+++ frontend/src/assets/reusableFunctions/imageCompressor.js
@@ -1,9 +1,10 @@
 // Código tomado en gran parte de https://stackoverflow.com/questions/51809241/how-to-compress-a-base64-image-to-custom-size
 
-const resize = async (img, type) => {
+// Función que recibe la imagen de tipo "new Image()" y su extensión (.jpg/.png)
+// y regresa la imagen con un tamaño muy cercano al max_size establecido
+const resize = async (img, type, MAX_SIZE) => {
 
-    const MAX_SIZE = 50000 // 50kb
-    const MIN_SIZE = 45000 // 45kb
+    const MIN_SIZE = MAX_SIZE*0.9 // 90% of max_size
 
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
@@ -24,35 +25,35 @@ const resize = async (img, type) => {
     
     if (blob.size < MAX_SIZE) return blob
     
-    // Binary search for the right size
+    // Binary search para encontrar el tamaño más cercano a los límites
     while (true) {
         const mid = Math.round( ((start + end) / 2) * 100 ) / 100
         if (mid === last || (blob.size < MAX_SIZE && blob.size > MIN_SIZE)) break
         last = mid
         blob = await new Promise(rs => canvas.toBlob(rs, 'image/'+type, mid))
-        console.log(`Quality set to ${mid} gave a Blob size of ${blob.size} bytes`)
         if (blob.size > MAX_SIZE) end = mid 
         if (blob.size < MAX_SIZE) start = mid
     }
 
     return blob
-  }
+}
 
-const imageCompressor = (base64imageStr) => {
+// Función que comprime la imagen al máximo tamaño de bytes que recibe en MAX_SIZE
+// ENTRADA: esta recibe la imagen en base64imageStr y el número de bytes en MAX_SIZE
+// SALIDA: regresa un string con la imagen en base64, EN FORMATO webp
+const imageCompressor = (base64imageStr, MAX_SIZE = 50000) => { // por defecto se establecen 50kb
     
     let imageObject = new Image()
     imageObject.src = base64imageStr
     
-    console.log('Converting image to webp\n')
+    // Se le indica a la función que regrese la imagen en formato 'webp' ya que es el formato que más cuida la calidad de imagen
+    resize(imageObject, 'webp', MAX_SIZE).then(blob => {
 
-    resize(imageObject, 'webp').then(blob => {
-        console.log('Final blob size', blob.size)
-
-        let reader = new FileReader();
-        reader.readAsDataURL(blob); 
+        let reader = new FileReader()
+        reader.readAsDataURL(blob)
         reader.onloadend = () => {
-            var base64data = reader.result;                
-            console.log('----- Final result -----:', base64data);
+            var base64data = reader.result           
+            console.log('----- Final result -----:', base64data); // __________ ¡¡¡¡¡¡¡QUITAAAAAR!!!!!!! __________
             return base64data
         }
     })
