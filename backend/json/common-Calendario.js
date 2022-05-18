ab9b4b325fe21f96a27a93a57a67c9766bf7c618
danie@LAPTOP-MQGLDRRG <a01734205tesm.mx>
2022-05-18 12:32:43 -0500

    Agrege Jsons de la pantallas de Calendario, funciona? no lo se, espero

---
+++ backend/json/common-Calendario.js
@@ -0,0 +1,139 @@
+/* {
+    "CampoMesAnio": [ 
+        {
+            "id": 1,
+           "Sup": "Abril",
+            "Inf": "2022"
+          }
+        ],
+  
+    "diasCalendario": [
+        {
+            "numeroDia": "7",
+            "isActive": "1",
+            "mes": "Abril",
+            "asesorias": [
+                {
+                    "status": "Activa",
+                    "hora": "09:00"
+  
+                },
+                {
+                    "status": "Cancelada",
+                    "hora": "10:00"
+                    
+                },
+                {
+                    "status": "Activa",
+                    "hora": "15:00"
+  
+                    
+                }
+            ]
+        },
+        {
+            "numeroDia": "8",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "9",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "10",
+            "isActive": "1",
+            "asesorias": [
+                {
+                    "status": "Activa",
+                    "hora": "08:00"
+                  
+                },
+                {
+                    "status": "Activa",
+                    "hora": "11:00"
+                },
+                {
+                    "status": "Activa",
+                    "hora": "17:00"
+                }
+            ]
+        },
+        {
+            "numeroDia": "11",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "14",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "15",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "16",
+            "isActive": "1",
+            "asesorias": [
+                {
+                    "status": "Finalizada",
+                    "hora": "08:00"
+                },
+                {
+                    "status": "Activa",
+                    "hora": "18:00"
+                }
+            ]
+        },
+        {
+            "numeroDia": "17",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "18",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "21",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "22",
+            "isActive": "1",
+  
+            "asesorias": [
+                {
+                    "status": "Activa",
+                    "hora": "13:00"
+                }
+            ]
+        },
+        {
+            "numeroDia": "23",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "24",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "25",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "28",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "29",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "30",
+            "isActive": "0"
+        },
+        {
+            "numeroDia": "31",
+            "isActive": "0"
+        }
+    ]
+  } */
