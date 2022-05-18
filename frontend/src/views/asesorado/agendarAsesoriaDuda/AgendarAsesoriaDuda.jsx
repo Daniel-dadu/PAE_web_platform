f3a00c8ca586aa0071d2472d97605a24f33ffdcc
Daniel-dadu <dadu9494@gmail.com>
2022-05-18 05:48:23 +0000

    Improved images compression code

--- frontend/src/views/asesorado/agendarAsesoriaDuda/AgendarAsesoriaDuda.jsx
+++ frontend/src/views/asesorado/agendarAsesoriaDuda/AgendarAsesoriaDuda.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from "react-router-dom"
 import ImageUploading from "react-images-uploading";
 import './AgendarAsesoriaDuda.css'
@@ -55,11 +55,70 @@ function AgendarAsesoriaDuda() {
   // El array imageList siempre va a tener la lista de las imágenes que se suban
   // y en imageList[index].data_url tendrá el src del archivo
 
-  const [images, setImages] = React.useState([]);
-  const onChange = (imageList) => {
-        // console.log(imageList);
+  const [images, setImages] = useState([]);
+  const onChangeImages = (imageList) => {
+      console.log(imageList);
       setImages(imageList);
   };
+
+
+  const resize = async (img, type) => {
+
+    const MAX_SIZE = 50000 // 50kb
+    const MIN_SIZE = 45000 // 45kb
+
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    
+    ctx.drawImage(img, 0, 0)
+    
+    let width = img.width
+    let height = img.height
+    let start = 0
+    let end = 1
+    let last, blob
+    
+    canvas.width = width
+    canvas.height = height
+    ctx.drawImage(img, 0, 0, width, height)
+    
+    blob = await new Promise(rs => canvas.toBlob(rs, 'image/'+type, 1))
+    
+    if (blob.size < MAX_SIZE) return blob
+    
+    // Binary search for the right size
+    while (true) {
+        const mid = Math.round( ((start + end) / 2) * 100 ) / 100
+        if (mid === last || (blob.size < MAX_SIZE && blob.size > MIN_SIZE)) break
+        last = mid
+        blob = await new Promise(rs => canvas.toBlob(rs, 'image/'+type, mid))
+        console.log(`Quality set to ${mid} gave a Blob size of ${blob.size} bytes`)
+        if (blob.size > MAX_SIZE) end = mid 
+        if (blob.size < MAX_SIZE) start = mid
+    }
+
+    return blob
+  }
+
+  const imageCompressor = (base64imageStr) => {
+    
+    let imageObject = new Image()
+    imageObject.src = base64imageStr
+    
+    console.log('Converting image to webp\n')
+
+    resize(imageObject, 'webp').then(blob => {
+        console.log('Final blob size', blob.size)
+
+        let reader = new FileReader();
+        reader.readAsDataURL(blob); 
+        reader.onloadend = () => {
+            var base64data = reader.result;                
+            console.log('----- Final result -----:', base64data);
+            return base64data
+        }
+    })
+}
 
   return (
     <AgendarAsesoria 
@@ -76,7 +135,7 @@ function AgendarAsesoriaDuda() {
                 <CampoTextoGrande/>
             </div>
                 
-            <ImageUploading multiple value={images} onChange={onChange} maxNumber={3} dataURLKey="data_url">
+            <ImageUploading multiple value={images} onChange={onChangeImages} maxNumber={3} dataURLKey="data_url" acceptType={['jpg', 'png']}>
                 {({ imageList, onImageUpload, onImageRemove }) => (
                 <div className="container_ImageUploading">
                     <div className='container_imagenes'>
@@ -106,6 +165,9 @@ function AgendarAsesoriaDuda() {
                 )}
             </ImageUploading>
 
+            <div>
+                <button onClick={() => imageCompressor(images[0].data_url)}> imageCompressor </button>
+            </div>
         </div>
     </AgendarAsesoria>
   )
