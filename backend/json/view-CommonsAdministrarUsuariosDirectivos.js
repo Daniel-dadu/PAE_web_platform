9ae3efbc37d6a106f6c57cb85bfabc93e37b1ee6
EzequielLG <A01734172@tec.mx>
2022-05-17 22:13:51 -0500

    Add JSON example for commonPanelNotificaciones #185 and commonAdministrarUsuariosDirectivos #189

---
+++ backend/json/view-CommonsAdministrarUsuariosDirectivos.js
@@ -0,0 +1,42 @@
+/*
+{
+    "usuarios": [
+         
+         {
+             "id": 1,
+             "nombreCompleto": "Daniel Esteban Maldonado Espitia"
+ 
+         },
+         {
+             "id": 2,
+             "nombreCompleto": "Daniel Munive Meneses"
+ 
+         },
+         {
+             "id": 3,
+             "nombreCompleto": "Daniel Flores Rodriguez"
+ 
+         },
+         {
+             "id": 4,
+             "nombreCompleto": "Ezequiel Lozano Guerrero"
+ 
+         },
+         {
+             "id": 5,
+             "nombreCompleto": "Fernando Jimenez"
+ 
+         },
+         {
+             "id": 6,
+             "nombreCompleto": "Emiliano Zapata"
+ 
+         },
+         {
+             "id": 5,
+             "nombreCompleto": "Leticia Rodríguez Aguilar"
+ 
+         }
+    ]
+}
+*/
---
+++ backend/json/view-CommonsPanelNotificaciones.js
@@ -0,0 +1,36 @@
+/*
+{
+    "notificaciones": [
+        {
+            "color": "verde",
+            "titulo": "Mensaje de PAE:",
+            "leyenda": "11:00 PM — 4:00 AM",
+            "contenido": "Tenemos reunión general el día lunes 30 de febrero en casa de Boba a las 9 pm. Vengan con toda la actitud para pasarla bien :)"
+        },
+        {
+            "color": "rojo",
+            "titulo": "Mensaje de PAE:",
+            "leyenda": "11:00 PM — 4:00 AM",
+            "contenido": "Tenemos reunión general el día lunes 30 de febrero en casa de Boba a las 9 pm. Vengan con toda la actitud para pasarla bien :)"
+        },
+        {
+            "color": "azul",
+            "titulo": "Mensaje de PAE:",
+            "leyenda": "11:00 PM — 4:00 AM",
+            "contenido": "Tenemos reunión general el día lunes 30 de febrero en casa de Boba a las 9 pm. Vengan con toda la actitud para pasarla bien :)"
+        },
+        {
+            "color": "azul",
+            "titulo": "Mensaje de PAE:",
+            "leyenda": "11:00 PM — 4:00 AM",
+            "contenido": "Tenemos reunión general el día lunes 30 de febrero en casa de Boba a las 9 pm. Vengan con toda la actitud para pasarla bien :)"
+        },
+        {
+            "color": "rojo",
+            "titulo": "Mensaje de PAE:",
+            "leyenda": "11:00 PM — 4:00 AM",
+            "contenido": "Tenemos reunión general el día lunes 30 de febrero en casa de Boba a las 9 pm. Vengan con toda la actitud para pasarla bien :)"
+        }
+    ]
+}
+*/
