c50dce2a9e7d5ad76850232c0e38316ec895a1e7
Daniel-dadu <dadu9494@gmail.com>
2022-05-21 00:39:30 +0000

    Commented calendario code #87

--- frontend/src/views/commons/calendario/Calendario.jsx
+++ frontend/src/views/commons/calendario/Calendario.jsx
@@ -12,8 +12,10 @@ import dateFunctions from '../../../assets/reusableFunctions/dateFunctions.js'
 
 function Calendario() {
 
+  // Variable para conocer la fecha de hoy y actualizar el año
   const [today, setToday] = useState(new Date())
 
+  // Variable de tipo objeto para actuazlizar los valores del mes y año
   const [mesAnio, setmesAnio] = useState(
     {
       mes: dateFunctions.getMonthEspanol(today.getMonth()),
@@ -21,6 +23,8 @@ function Calendario() {
     }
   )
 
+  // Función que maneja se ejecuta cuando se da click a una flecha del componente CambioMesPeriodo
+  // Recibe como parámetro el tipo de botón al que se le dió click y cambia el valor de 'mesAnio' y 'today'
   const handleArrowClick = arrow => {
     let currentMonth = mesAnio.mes
     if(currentMonth === 'Diciembre' && arrow === 'next') {
