9d260a0356d8ed8a19f73185af7eb8422463de6d
Daniel-dadu <dadu9494@gmail.com>
2022-05-21 06:18:33 +0000

    Improved code in new component ImagenPerfilCambiar

--- frontend/src/components/reusable/ImagenPerfilCambiar/ImagenPerfilCambiar.jsx
+++ frontend/src/components/reusable/ImagenPerfilCambiar/ImagenPerfilCambiar.jsx
@@ -1,47 +1,46 @@
 import React, { useState } from 'react'
 
-import { ImagenAsesoria, BotonConImagen } from '../../../routeIndex'
+import { BotonConImagen } from '../../../routeIndex'
 
 import './ImagenPerfilCambiar.css'
 
 import ImageUploading from "react-images-uploading";
 import { BiImageAdd } from 'react-icons/bi'
+import { TiDelete } from 'react-icons/ti'
 import noUserImg from '../../../assets/noUserImg.png'
 
 function ImagenPerfilCambiar() {
 
-    const [image, setImage] = useState(noUserImg);
-    const onChange = (img) => setImage(img)
+    const [image, setImage] = useState([])
+    const onChange = (imageList) => setImage(imageList)
 
     return (
         <div>
             <ImageUploading value={image} onChange={onChange} maxNumber={1} dataURLKey="data_url">
-                {({ imageUser, onImageUpload, onImageRemove }) => (
+                {({ imageList, onImageUpload, onImageUpdate, onImageRemove }) => (
                 <div className="container_ImageUploading_cambiar">
-                    <div className='container_imagenes_RegistroAsesorado'>
-                        {/* {imageList.length === 0 ?  */}
-                        <img src={imageUser} alt="Sin imagen" />
-                        {/* : imageList.map((image, index) => (
-                            <ImagenAsesoria
-                                allowClosed = '1'
-                                onClickX = {() => onImageRemove(index)}
-                                size = 'reducida'
-                                source = {image.data_url}
-                                alt = {`ImagenAsesoria${index}`}
-                                nameDownloadImage = {`ImagenAsesoria${index}`}
-                            />
-                        ))} */}
-                        <button onClick={() => onImageRemove()}>Eliminar foto</button>
+                    <div className='container_image_selected'>
+                        { imageList.length === 0 ? 
+                            <img src={noUserImg} alt="Foto subida" className='foto_subida_usuario'/>
+                            :
+                            imageList.map((image, index) => (
+                                <div>
+                                    <img src={image.data_url} alt="Foto subida" className='foto_subida_usuario'/>
+                                    <button onClick={() => onImageRemove(index)} className='btn_delete_userImg'>
+                                        <TiDelete size={30}/>
+                                    </button>
+                                </div>
+                            ))
+                        }
                     </div>
 
                     <div className='btn_upload'>
                         <BotonConImagen 
-                        // onClick={imageList.length === 1 ? () => alert('No se permite subir más de 1 imagen') : onImageUpload} 
-                        onClick={onImageUpload} 
-                        backgroundColor="azulCielo" 
-                        size="largo" 
+                        onClick={imageList.length === 0 ? onImageUpload : () => onImageUpdate(0)} 
+                        backgroundColor="turquesa" 
+                        size="reducido" 
                         Image={BiImageAdd} >
-                            Subir imagen
+                            {imageList.length === 0 ? "Subir imagen" : "Cambiar imagen"}
                         </BotonConImagen>
                     </div>
                 </div>
