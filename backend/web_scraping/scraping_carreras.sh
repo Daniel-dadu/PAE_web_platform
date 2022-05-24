7f8144dd12c5c461165db17937daa40724f0bda8
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-05-24 18:12:45 -0500

    Unidades de formacion de carreras filtradas de lineas con '(+)'

--- backend/web_scraping/scraping_carreras.sh
+++ backend/web_scraping/scraping_carreras.sh
@@ -4,3 +4,5 @@ siglas="./carreras/carreras_siglas.txt"
 nombres="./carreras/carreras_nombres.txt"
 links="./carreras/carreras_links.txt"
 
+#Filtra la lista de ufs de las carreras eliminando las lineas que continenen '(+)'
+#gawk -i inplace '!/[()]/' ./carrera/*
