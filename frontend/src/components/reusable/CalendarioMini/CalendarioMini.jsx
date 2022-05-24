608b27e5418aac6e91440752f806893995612d03
Daniel-dadu <dadu9494@gmail.com>
2022-05-24 16:33:26 +0000

    Connected calendario mini to backend so it uses the available days from there

--- frontend/src/components/reusable/CalendarioMini/CalendarioMini.jsx
+++ frontend/src/components/reusable/CalendarioMini/CalendarioMini.jsx
@@ -59,26 +59,24 @@ function makeCalendarioMini(calendar, year, monthIndex){
 
 
 /*
-    El componente recibe 3 valores.
-        -enabledDays: un json con el nombre del mes correspodiante al index como 
-        llave, y una lista de los dias habiles.
-            Ejemplo:
-                {
-                    "Abril": [1, 2, 3, 5, 20, 21, 30, 31]
-                }
+El componente recibe 3 valores.
+    -enabledDays: un json con el nombre del mes correspodiante al index como 
+    llave, y una lista de los dias habiles.
+        Ejemplo:
+            {
+                "Abril": [1, 2, 3, 5, 20, 21, 30, 31]
+            }
 
-        -year: el año actual.
-        -montIndex: El mes que se desea mostrar, por defecto es 0, y no puede 
-        ser negativo.
-        -minMonth: El primer mes que debe mostrar el minicalendario
-        -maxMonth: El ultimo mes que debe mostrar el minicalendario
+    -year: el año actual.
+    -montIndex: El mes que se desea mostrar, por defecto es 0, y no puede 
+    ser negativo.
+    -minMonth: El primer mes que debe mostrar el minicalendario
+    -maxMonth: El ultimo mes que debe mostrar el minicalendario
 
-    Ejemplo:
-        enabledDays={info} year="2022" monthIndex={3} minMonth={0} maxMonth={4}
-    */
-function CalendarioMini(/* {enabledDays, year, monthIndex, minMonth, maxMonth} */){
-
-    const today = new Date()
+Ejemplo:
+    enabledDays={info} year="2022" monthIndex={3} minMonth={0} maxMonth={4}
+*/
+function CalendarioMini(){
 
     const navigate = useNavigate()
 
@@ -89,71 +87,56 @@ function CalendarioMini(/* {enabledDays, year, monthIndex, minMonth, maxMonth} *
         }
     })
     
+    const today = new Date()
+
     const year = today.getFullYear() 
-    const [month, setMonth] = useState(today.getMonth());
+    const [month, setMonth] = useState(today.getMonth())
 
-    const minMonth=0
-    const maxMonth=4
+    const minMonth = 2
+    const maxMonth = 5
 
-    // const [enabledDays, setEnabledDays] = useState(requestDias(today.getMonth()))
+    // Variable en la que se guarda la respuesta de la API con el nombre del mes y el array de días disponibles
     const [enabledDays, setEnabledDays] = useState({'Cargando': []})
 
+    // Variable que guarda el código HTML del calendario que regresa la función makeCalendarioMini
     const [miniCalendario, setMiniCalendario] = useState(makeCalendarioMini(enabledDays, year, month));
+
+    // Variable que guarda el nombre del mes en string, el cual obtiene de enabledDays
     const [monthName, setMonthName] = useState(Object.keys(enabledDays));
 
-    const requestDias = (mes) => {  
+    useEffect(() => {  
         const config = {
             method: 'get',
-            url: `http://20.225.209.57:3094/asesoria/get_dias/?uf=${localStorage.asesoria_uf}&anio=${year}&mes=${mes}`,
+            url: `http://20.225.209.57:3094/asesoria/get_dias/?uf=${localStorage.asesoria_uf}&anio=${year}&mes=${month}`,
             headers: { }
         }
         
         axios(config)
-        .then(function (response) {
-            console.log(response.data);
+        .then(response => {
             setEnabledDays(response.data);
             setMonthName(Object.keys(response.data))
+            setMiniCalendario(makeCalendarioMini(response.data, year, month-1));
         })
-        .catch(function (error) {
+        .catch(error => {
             alert("Error con API")
             console.log(error);
-        });
+        })
 
-        // try {
-        //     const response = await axios(config)
-        //     console.log(response.data);
-        //     setEnabledDays(response.data);
-        // } catch (error) {
-        //     alert("Error con API")
-        //     console.log(error);
-        // }
-
-    }
-
-    
+    }, [year, month, setEnabledDays, setMonthName, setMiniCalendario])
 
     const goLastMonth = () => {
-        if(month > minMonth){
-            setMonth(month - 1);
-            let test = {"Marzo": [1,3,5,20,21,22,23]};
-            setMiniCalendario(makeCalendarioMini(test, year, month -1));
-            setMonthName(Object.keys(test));
-        }
-    };
+        if(month >= minMonth) setMonth(month - 1)
+    }
+
     const goNextMonth = () => {
-        if(month < maxMonth){
-            setMonth(month + 1);
-            //llamada al back para establecer nuevos enables days
-            setMiniCalendario(makeCalendarioMini(enabledDays, year, month +1));
-            setMonthName(Object.keys(enabledDays));
-        }
-    };
+        if(month <= maxMonth) setMonth(month + 1)
+    }
 
     return(
         <div className='calendario_mini_container'>
             <div className='calendario_mini_top'>
                 <button type="button" className='buttonArrow' onClick={goLastMonth}><i className="arrow left"></i></button>
-                <span>{monthName} - {new Date().getFullYear()}</span>
+                <span>{monthName} - {year}</span>
                 <button type="button" className='buttonArrow' onClick={goNextMonth}><i className="arrow right"></i></button>
             </div>
             <table className='calendario_mini_month'>
@@ -171,9 +154,9 @@ function CalendarioMini(/* {enabledDays, year, monthIndex, minMonth, maxMonth} *
                 </tbody>
             </table>
 
-            <div>
+            {/* <div>
                 <button onClick={() => requestDias(4)}> request </button>
-            </div>
+            </div> */}
         </div>
     )   
 }
