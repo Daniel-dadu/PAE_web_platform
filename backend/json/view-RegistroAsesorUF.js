30ce76911fea732e0a526eecc46f2228bf609d33
EzequielLG <A01734172@tec.mx>
2022-05-25 07:43:43 -0500

    Add JSON of view RegistroAsesorUF

---
+++ backend/json/view-RegistroAsesorUF.js
@@ -0,0 +1,75 @@
+/*
+{
+    "planEstudios": [
+        {
+            "semestre": "1",
+            "unidadesFormacion": [
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B", // DANO debe cambiar la forma del JSON de modo que la clave de la materia esté en 'claveUF' y no en 'horaAsesoria'
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                }
+            ]
+        },
+        {
+            "semestre": "2",
+            "unidadesFormacion": [
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                }
+            ]
+        },
+        {
+            "semestre": "3",
+            "unidadesFormacion": [
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                },
+                {
+                    "claveUF":"",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"TC3005B",
+                    "contenido":"Desarrollo de software"
+                }
+            ]
+        }
+    ]
+}
+*/
