4d2a9a0d402c51eab5276adfd3413e0977784417
Daniel Flores Rodríguez <danoman@Daniels-MacBook-Air.local>
2022-05-25 08:29:30 -0500

    Finish view-PerfilAsesorado #198

--- frontend/src/views/asesorado/perfil/PerfilAsesorado.jsx
+++ frontend/src/views/asesorado/perfil/PerfilAsesorado.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Template, InformacionPersonalUsuario, BotonConImagen } from '../../../routeIndex';
+import './perfilAsesorado.css';
+import { BsBoxArrowInRight } from 'react-icons/bs'
+
+
+
 
 function PerfilAsesorado() {
   return (
-    <div>PerfilAsesorado</div>
+      <>
+        <Template view={"perfil"} >
+          <div className = 'btn_PerfilCommon'>
+
+            <h1> Perfil </h1>
+
+            <div className = 'botonCerrarSesion'>
+                <BotonConImagen 
+                    onClick = {'Hola'} 
+                    backgroundColor = 'grisClaro'
+                    size = "grande" 
+                    Image = {BsBoxArrowInRight}
+                >
+                    Cerrar Sesión
+                </BotonConImagen>
+            </div>
+
+          </div>
+        
+          <div className='contenedor-informacion-perfilAsesorado'>
+            <InformacionPersonalUsuario />
+          </div>
+        </Template>
+      </>
   )
 }
 
