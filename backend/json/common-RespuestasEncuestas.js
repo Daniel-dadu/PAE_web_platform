bc42164f01c7d4ab905a9774e80701711501f4ec
EzequielLG <A01734172@tec.mx>
2022-05-24 20:47:37 -0500

    Add JSON of common-RespuestasEncuestas

---
+++ backend/json/common-RespuestasEncuestas.js
@@ -0,0 +1,123 @@
+/*
+{
+    "tipoUsuario": "asesor", // asesor/asesorado
+    "usuarios": [
+        {
+            "nombreUsuario": "Ezequiel Lozano Guerrero", // Nombre del asesor/asesorado
+            "asesorias": [
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Daniel Maldonado Espitia fue el asesorado" // Escribir 'fue el asesorado' o 'fue el asesor', según dependa
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 2 fue la asesorada"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 3 fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 4 fue la asesorada"
+                }
+            ]
+        },
+        {
+            "nombreUsuario": "Reed Richards",
+            "asesorias": [
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Ezequiel Lozano Guerrero fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 2 fue la asesorada"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 3 fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 4 fue la asesorada"
+                }
+            ]
+        },
+        {
+            "nombreUsuario": "Julio Gabriel Verne",
+            "asesorias": [
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Daniel Maldonado Espitia fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 2 fue la asesorada"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 3 fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 4 fue la asesorada"
+                }
+            ]
+        },
+        {
+            "nombreUsuario": "Monica Rambeau",
+            "asesorias": [
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Ezequiel Lozano Guerrero fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 2 fue la asesorada"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 3 fue el asesorado"
+                },
+                {
+                    "claveUF":"TC3005B",
+                    "colorTipo3":"blanco_tipo3",
+                    "horaAsesoria":"7/Marzo - 11 AM",
+                    "contenido":"Nombre persona 4 fue la asesorada"
+                }
+            ]
+        }
+    ]
+}
+*/
