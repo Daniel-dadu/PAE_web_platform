132d2ee04d6dd7302b74b3808ded781656d10186
EzequielLG <A01734172@tec.mx>
2022-05-24 23:28:32 -0500

    Add JSON example for componente-CampoTextoMenuDesplegable

---
+++ backend/json/componente-CampoTextoMenuDesplegable.js
@@ -0,0 +1,36 @@
+/* 
+DANO debe crear en el componente ListaDesplegable una manera de recibir un JSON específico
+para el uso de este componente, sin embargo, por el momento se puede utilizar así:
+
+{
+  "usuarios": [
+    {
+      "claveUF":"A01734172",
+      "colorTipo3":"gris_tipo_1",
+      "horaAsesoria":"Ezequiel",
+      "contenido":"Lozano"
+    },
+    {
+      "claveUF":"A01734172",
+      "colorTipo3":"gris_tipo_1",
+      "horaAsesoria":"Ezequiel",
+      "contenido":"Lozano"
+    },
+    {
+      "claveUF":"A01734172",
+      "colorTipo3":"gris_tipo_1",
+      "horaAsesoria":"Ezequiel",
+      "contenido":"Lozano"
+    },
+    {
+      "claveUF":"A01734172",
+      "colorTipo3":"gris_tipo_1",
+      "horaAsesoria":"Ezequiel",
+      "contenido":"Lozano"
+    }
+  ]
+}
+
+Como se puede notar, es incoherente que claveUF sea la matrícula, horaAsesoria el nombre y contenido el apellido del alumno,
+por ello el componente que hizo DANO de ListaDesplegable debe modificarlo para después hacer las correcciones correspondientes
+*/
