0d22b798f12dbe2dac2a8da0362f191dba567072
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-05-31 10:28:54 -0500

    CLI Tool para levantar los microservicios basados en node

--- backend/microservices/CLI/CLI.sh
+++ backend/microservices/CLI/CLI.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 stty -echoctl # hide ^C
-# trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
 function ctrl_c() {
-    echo
-    echo "Trapped CTRL-C"
+    end_all
+    exit 0
 }
 
 function check_in_ignore(){
@@ -35,18 +34,54 @@ function search_services(){
     done
 }
 
-function start(){
+function install_all(){
     for SERVICE in ${MICROSERVICES[@]}; do
-        echo "Starting ${SERVICE::-1}"
+        echo "Installing ${SERVICE::-1}"
         cd $SERVICE
-        ls
+        npm install
         cd $WORKING_DIR
+    done
+}
+
+function start_all(){
+    for SERVICE in ${MICROSERVICES[@]}; do
+        local SERVICE_NAME=${SERVICE::-1}
+        local SERVICE_PID=""
+
+        echo "Starting $SERVICE_NAME"
+        cd $SERVICE
+        
+        npm start&
+        SERVICE_PID=$!
+        echo -e "::\tPID $SERVICE_PID"
+        MICROSERVICES_PIDS+=($SERVICE_PID)
+
+        cd $WORKING_DIR
+    done
+}
+
+function end_all(){
+    for SERVICE in ${!MICROSERVICES_PIDS[@]}; do
+        echo "Stopping ${MICROSERVICES[$SERVICE]::-1}"
+        kill ${MICROSERVICES_PIDS[$SERVICE]}
+    done
+}
+
+function main(){
+
+    search_services
+    install_all
+    start_all
+
+    echo "PRESS CTRL-C to exit"
+
+    while true; do
+        :
     done
 }
 
 WORKING_DIR=$PWD
 MICROSERVICES=()
+MICROSERVICES_PIDS=()
 
-search_services
-echo ${MICROSERVICES[@]}
-#start
+main
