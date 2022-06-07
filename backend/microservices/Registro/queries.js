f2ba9bfa4576659cb8bb6b8208c53e760b431aa2
Daniel-dadu <dadu9494@gmail.com>
2022-06-07 03:22:21 +0000

    Adding endpoint for asesor register #292

--- backend/microservices/Registro/queries.js
+++ backend/microservices/Registro/queries.js
@@ -74,6 +74,74 @@ const nuevo_asesorado = (request, response) => {
     })
 }
 
+const nuevo_asesor = (request, response) => {
+    const matricula = request.body.matricula
+    const contrasena = request.body.contrasena
+    const nombre = request.body.nombre
+    const apellidoPaterno = request.body.apellidoPaterno
+    const apellidoMaterno = request.body.apellidoMaterno
+    const fotoPerfil = request.body.fotoPerfil
+    const telefono = request.body.telefono
+    const carrera = request.body.carrera
+    const carrera2 = request.body.carrera2
+
+    const horario1 = request.body.horarioPeriodo1
+    const horario2 = request.body.horarioPeriodo2
+    const horario3 = request.body.horarioPeriodo3
+
+    const rollbackTransaction = () => {
+        pool.query('ROLLBACK', error => {
+            if(error) 
+                response.status(400).send('Error: No se pudo detener la transacción en el proceso de registro. Probablemente se haya registrado parcialmente al asesor')
+            else 
+                response.status(409).send('Error: No se pudo registrar al asesor, probablemente la matrícula ya está resgitrada (transacción cancelada correctamente)')
+        })
+    }
+
+    pool.query('BEGIN', error => {
+        if(error) rollbackTransaction()
+        else {
+
+            // JSON Checklist para ir indicando que se registró correctamente cada parte
+            let checklistRegistro = {
+                datosPerfil: false,
+                horario: false,
+                UFs: false
+            }
+
+            // Obtenemos el nuevo salt y la contraseña encriptada
+            const salt = encrypt.getSalt()
+            const password = encrypt.getPassword(contrasena, salt)
+            
+            const consulta = `CALL registro_datosperfil_asesor($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`
+            const params = [matricula, password, salt, nombre, apellidoPaterno, apellidoMaterno, fotoPerfil, telefono, carrera, carrera2]
+
+            pool.query(consulta, params, error => {
+                if(error){
+                    rollbackTransaction()
+                    return
+                } else {
+                    checklistRegistro.datosPerfil = true
+                }
+            })
+
+            let periodos
+
+            pool.query(`SELECT "numero", "fechaInicial", "fechaFinal" FROM "Periodo" WHERE "status" = 'actual'`, (error, results) => {
+                if(error){
+                    rollbackTransaction()
+                    return
+                } else {
+                    periodos = results.rows
+                }
+            })
+
+
+
+        }
+    })
+
+}
 
 module.exports = {
     prueba_fotoPerfil,
