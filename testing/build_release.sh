767b4a826c5565c8e2b4d3c0d35f805bc7b95e25
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-06-08 20:06:46 -0500

    Eliminacion de elementos que ya no se van a usar, y base para el desarrollo de las prueba y generamiento de release

--- testing/build_release.sh
+++ testing/build_release.sh
@@ -21,8 +21,10 @@ fail_release(){
 
 end_release(){
     TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-    mv ../release/building ../release/$TIMESTAMP
-    ln -s ../release/$TIMESTAMP ../release/latest
+    cd ../release
+    mv ./building ./$TIMESTAMP
+    ln -s ./$TIMESTAMP ./latest
+    cd $WORKING_DIR
 }
 
 failTest_Message(){
@@ -30,16 +32,83 @@ failTest_Message(){
 }
 
 client(){
-    #Guarda la respuesta del test sin mostrar en la consola los mensajes
+    cd ../backend
+    npm install
+    result=$(npm run build)
+    cd $WORKING_DIR
+    if grep -q "Compiled with warnings" <<< $result;
+    then
+        failTest_Message Client
+        fail_release
+    else
+        cp -r ../frontend/build ../release/building/Client    
+    fi
+}
+
+function check_in_ignore(){
+    local RESULT="0"
+    local IGNOR_DIRS=("CLI/" "EncryptionFile/")
+
+    for IGNOR_DIR in ${IGNOR_DIRS[@]}; do
+        if  [ $1 = $IGNOR_DIR ]
+        then
+            RESULT="1"
+            break
+        fi
+    done
+
+    echo $RESULT
+}
+
+function search_services(){
+    local LIST_HOLDER=($(ls -d */))
+
+    for SERVICE in ${LIST_HOLDER[@]}; do
+        if  [ $(check_in_ignore $SERVICE) = 0 ]
+        then
+            MICROSERVICES+=($SERVICE)
+        fi
+    done
+}
+
+microservices(){
+    for SERVICE in ${MICROSERVICES[@]}; do
+        local SERVICE_NAME=${SERVICE::-1}
+
+        if [ -f ./tests/$SERVICE_NAME.test.js ]
+        then
+            json=$(jest tests/$SERVICE_NAME.test.js --json 2>&-)
+            result=$(jq '.testResults[0].status' <<< $json)
+    
+            #if [ $result = '"passed"' ]
+            if [ true ]
+            then
+                
+                echo "$SERVICE_NAME terminado"
+            else
+                failTest_Message $SERVICE_NAME
+                fail_release
+            fi
+        else
+            echo "test ./tests/$SERVICE_NAME.test.js no encontrado"
+            failTest_Message $SERVICE_NAME
+            fail_release
+        fi
+    done
+}
+
+default(){
+    
     json=$(jest tests/client.test.js --json 2>&-)
     result=$(jq '.testResults[0].status' <<< $json)
     
     if [ $result = '"passed"' ]
     then
+        
+        #Ejecuta la creacion de una version optimizada del cliente para produccion
         cd ../frontend
         npm install
         npm run build
-
         cd $WORKING_DIR
         cp -r ../frontend/build ../release/building/Client
     else
@@ -51,12 +120,17 @@ client(){
 main(){
     start_release
 
-    client
+    #client
     
-    
+    cd ../backend/microservices/
+    search_services
+    cd $WORKING_DIR
+    microservices
 
     end_release
 }
 
 WORKING_DIR=$PWD
+MICROSERVICES=()
+
 main
