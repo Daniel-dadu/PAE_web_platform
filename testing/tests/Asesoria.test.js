6379e83d3003e674c7c41275fdfcef616602a2b8
Fernando Jimenez Pereyra <a01734609@tec.mx>
2022-06-09 16:33:09 -0500

    Base para generar los test

--- testing/tests/Asesoria.test.js
+++ testing/tests/Asesoria.test.js
@@ -1,16 +1,33 @@
-const express = require('express')
-const app = express()
-const supertest = require('supertest')
-const request = supertest(app)
+const request = require('supertest')
+const baseUrl = 'https://jsonplaceholder.typicode.com/';
 
 describe('Lista de unidades de formacion', () => {
 
-  test('GET / UF de ITC de primer semestre', async () => {
-    //const data = await request.get('http://20.225.209.57:3094/asesoria/get_uf/?carrera=ITC&semestre=1')
-    const data = await request.get('https://jsonplaceholder.typicode.com/todos/')
-    console.log(data)
-    console.log(data.status)
-    console.log(data.body)
-    expect(data).not.toBe(0);
-  });
+    test('GET / UF de ITC de primer semestre', async () => {
+        //const data = await request.get('http://20.225.209.57:3094/asesoria/get_uf/?carrera=ITC&semestre=1')
+        await request(baseUrl)
+            .get('todos/')
+            .expect(200);
+    });
+
+    test('GET / UF de ITC de primer semestre content', async () => {
+        //const data = await request.get('http://20.225.209.57:3094/asesoria/get_uf/?carrera=ITC&semestre=1')
+        const response = await request(baseUrl)
+            .get('todos/');
+        
+            console.log(response.body)
+            expect(response.statusCode).toBe(200);
+    });
 });
+
+80
+3000
+3014
+3090
+3091
+3092
+3093
+3094
+3095
+3030
+3031
